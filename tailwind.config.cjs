aad3b608b1cd3021109eb888da93883ccee303d2
Damilola Omifare <omifaredammy@gmail.com>
2023-02-26 21:33:25 +0000

    feat(dev): add tailwindcss forms

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,5 +5,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+  ],
 };
