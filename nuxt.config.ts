6c7f479ab967329990a1726983ce8d18e8d908ef
Damilola Omifare <omifaredammy@gmail.com>
2023-02-28 12:35:55 +0000

    feat: add google font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
   app: {
     head: {
       title:
         "Nuxity | Opinionated Nuxt3 Minimal Starter template packed with tailwindcss, google fonts, tailwind aspect ratio & more",
       titleTemplate: "%s - Nuxity",
+    },
+  },
+
+  // https://google-fonts.nuxtjs.org/
+  googleFonts: {
+    display: "swap",
+    families: {
+      Montserrat: {
+        wght: [300, 400, 500, 700],
+        ital: [400, 500, 700],
+      },
     },
   },
 });
